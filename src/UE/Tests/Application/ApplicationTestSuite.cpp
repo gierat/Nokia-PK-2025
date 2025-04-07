8dc86c06559b302c999b97738683dea5047f8965
Tomasz <gieratdoc@gmail.com>
2025-04-07 23:05:15 +0200

    Attach to BTS

--- src/UE/Tests/Application/ApplicationTestSuite.cpp
+++ src/UE/Tests/Application/ApplicationTestSuite.cpp
@@ -17,10 +17,13 @@ class ApplicationTestSuite : public Test
 {
 protected:
     const common::PhoneNumber PHONE_NUMBER{112};
+    const common::BtsId BTS_ID{1337};
     NiceMock<common::ILoggerMock> loggerMock;
     StrictMock<IBtsPortMock> btsPortMock;
     StrictMock<IUserPortMock> userPortMock;
     StrictMock<ITimerPortMock> timerPortMock;
+
+    Expectation showNotConnected = EXPECT_CALL(userPortMock, showNotConnected());
 
     Application objectUnderTest{PHONE_NUMBER,
                                 loggerMock,
@@ -30,10 +33,30 @@ protected:
 };
 
 struct ApplicationNotConnectedTestSuite : ApplicationTestSuite
-{};
-
-TEST_F(ApplicationNotConnectedTestSuite, todo)
 {
-}
+    void sendAttachRequestOnSib(){
+        using namespace std::chrono_literals;
+        EXPECT_CALL(btsPortMock, sendAttachRequest(BTS_ID));
+        EXPECT_CALL(timerPortMock, startTimer(500ms));
+        EXPECT_CALL(userPortMock, showConnecting());
+        objectUnderTest.handleSib(BTS_ID);
+    }
+};
 
+TEST_F(ApplicationNotConnectedTestSuite, shallSendAttachRequestOnSib)
+{
+    sendAttachRequestOnSib();
 }
+    struct ApplicationConnectingTestSuite : ApplicationNotConnectedTestSuite{
+    ApplicationConnectingTestSuite(){
+        sendAttachRequestOnSib();
+    }
+
+};
+
+    TEST_F(ApplicationConnectingTestSuite, shallConnectOnAttachAccept) {
+        EXPECT_CALL(timerPortMock, stopTimer());
+        EXPECT_CALL(userPortMock, showConnected());
+        objectUnderTest.handleAttachAccept();
+    }
+};
